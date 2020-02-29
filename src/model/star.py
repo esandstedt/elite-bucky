b89aa02a09f0cf8d840ffc58f10d26f8aab41874
Erik Sandstedt <erik@sandstedt.eu>
2020-02-29 14:51:38 +0100

    don't care about stars far from the primary

--- src/model/star.py
+++ src/model/star.py
@@ -2,14 +2,22 @@ import math
 
 
 class Star:
-    def __init__(self, id, name, x, y, z, distance_to_neutron, distance_to_scoopable):
+    def __init__(self, id, name, x, y, z, distance_to_neutron=None, distance_to_scoopable=None):
         self.id = id
         self.name = name
         self.x = x
         self.y = y
         self.z = z
-        self.distance_to_neutron = distance_to_neutron
-        self.distance_to_scoopable = distance_to_scoopable
+
+        if distance_to_neutron is not None and distance_to_neutron < 500:
+            self.distance_to_neutron = distance_to_neutron
+        else:
+            self.distance_to_neutron = None
+
+        if distance_to_scoopable is not None and distance_to_scoopable < 500:
+            self.distance_to_scoopable = distance_to_scoopable
+        else:
+            self.distance_to_scoopable = None
 
     def dist(self, other):
         return math.sqrt(self.dist_squared(other))
